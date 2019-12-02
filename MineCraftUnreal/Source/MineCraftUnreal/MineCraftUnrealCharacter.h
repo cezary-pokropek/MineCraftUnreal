ad4ce47d8de4f2b5f545f8f08d89db51dedea990
Cezary-Pokropek <cezary.pokropek@gmail.com>
2019-12-02 15:17:01 +0100

    Added PickAxe to the game

--- MineCraftUnreal/Source/MineCraftUnreal/MineCraftUnrealCharacter.h
+++ MineCraftUnreal/Source/MineCraftUnreal/MineCraftUnrealCharacter.h
@@ -19,7 +19,7 @@ class AMineCraftUnrealCharacter : public ACharacter
 
 	/** Gun mesh: 1st person view (seen only by self) */
 	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
-	class USkeletalMeshComponent* FP_Gun;
+	class USkeletalMeshComponent* FP_PickAxe;
 
 	/** Location on gun mesh where projectiles should spawn. */
 	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
